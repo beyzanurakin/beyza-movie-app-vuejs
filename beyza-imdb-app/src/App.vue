7eab4706113cd99884f41da71d9eae7ed8b77bad
beyzanurakin <akinbeyzanur@gmail.com>
2024-09-16 22:10:25 +0300

    Add Vue-router

--- beyza-imdb-app/src/App.vue
+++ beyza-imdb-app/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HomePage/>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/favorites">Favorites</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HomePage from './views/HomePage.vue'
-
 export default {
   name: 'App',
-  components: {
-    HomePage
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+nav {
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  background-color: #f8f9fa;
+}
+
+nav a {
+  margin: 0 15px;
+  text-decoration: none;
+  color: #007bff;
 }
 </style>
