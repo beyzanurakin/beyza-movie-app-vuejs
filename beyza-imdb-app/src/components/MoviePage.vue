4ccda4bc4d3f32e8e1f636eb1d69d6a47e2c3ee4
beyzanurakin <akinbeyzanur@gmail.com>
2025-01-10 01:08:21 +0300

    Updated fetchMovies method to include isFavourite property for movies

---
+++ beyza-imdb-app/src/components/MoviePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Movie Page</h1>
+      <p>Movie ID: {{ movieId }}</p>
+      <!-- Film bilgilerini burada gösterin -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        movieId: this.$route.params.id,
+      };
+    },
+  };
+  </script>
